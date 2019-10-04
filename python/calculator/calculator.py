f1a4c9089ddc08e9b5fc827f38958a1347f67067
joro2404 <georgi@Georgi.computer>
2019-10-04 10:35:02 +0300

    try

---
+++ python/calculator/calculator.py
@@ -0,0 +1,63 @@
+
+class Callculator():
+
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+
+    def add(self):
+        return self.a+self.b
+
+    def mul(self):
+        return self.a*self.b
+
+    def div(self):
+        return self.a/self.b
+
+    def sub(self):
+        return self.a-self.b
+
+while(1):
+    arr = [i for i in input().split()]
+    obj=Callculator(int(arr[0]), int(arr[2]))
+
+    if arr[1] == '+':
+        print("Result: ",obj.add())
+    if arr[1] == '-':
+        print("Result: ",obj.sub())
+    if arr[1] == '*':
+        print("Result: ",obj.mul())
+    if arr[1] == '/':
+        print("Result: ",obj.div())
+
+#print(arr)
+
+
+
+
+
+'''
+choice=1
+while choice!=0:
+    print("0. Exit")
+    print("1. Add")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    choice=int(input("Enter choice: "))
+    if choice==1:
+        print("Result: ",obj.add())
+    elif choice==2:
+        print("Result: ",obj.sub())
+    elif choice==3:
+        print("Result: ",obj.mul())
+    elif choice==4:
+        print("Result: ",round(obj.div(),2))
+    elif choice==0:
+        print("Exiting!")
+    else:
+        print("Invalid choice!!")
+
+'''
+ 
+ 
