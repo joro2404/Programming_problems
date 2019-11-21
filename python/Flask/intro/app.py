162d99369a533b058cd4b2d88360079b70e39ff5
joro2404 <joro2404@abv.bg>
2019-11-21 11:01:05 +0200

    whole week without commit

--- python/Flask/intro/app.py
+++ python/Flask/intro/app.py
@@ -1,27 +1,31 @@
 from flask import Flask
 from flask import render_template, redirect, url_for, request
 from post import Post
+import sqlite3
 
 app = Flask(__name__)           
 
+conn = sqlite3.connect('example.db')
 
 
-db = {
-    1:Post(1, 'post 1', 'Gosho,', 'Content')
-    
-    }
+conn.cursor().execute('CREATE TABLE IF NOT EXISTS posts(id  INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, author TEXT, content TEXT)')
+conn.commit()
+
 
 @app.route("/")
 def hello():               
-    return "Hello World!"
+    return redirect(url_for('list_posts'))
 
 @app.route('/posts')
 def list_posts():
-    return render_template('posts.html', posts = db.values())
+    conn = sqlite3.connect('example.db')
+    rows = conn.cursor().execute('SELECT * FROM posts').fetchall()
+    posts = [Post(*row) for row in rows]
+    return render_template('posts.html', posts = posts)
 
 @app.route('/posts/<int:id>')
 def show_post(id):
-    post = db[id]
+    post = Post.find(id)
     return render_template('post.html', post=post)
 
 @app.route('/posts/new', methods = ['GET', 'POST'])
@@ -29,7 +33,6 @@ def new_post():
     if request.method == 'GET':
         return render_template('new_post.html')
     elif request.method == 'POST':
-        next_id = max(db.keys())+1
-        post = (next_id, request.form['name'], request.form['author'], request.form['content'])
-        db[next_id] = post
+        values = (None, request.form['name'], request.form['author'], request.form['content'])
+        Post(*values).create()
         return redirect(url_for('list_posts'))
---
+++ python/Flask/intro/database.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect('example.db')
+
+DB_NAME = 'example.db'
+conn.cursor().execute('CREATE TABLE IF NOT EXISTS posts(id  INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, author TEXT, content TEXT)')
+
+conn.commit()
+
+class DB:
+    def __enter__(self):
+        self.conn = sqlite3.connect(DB_NAME)
+        return self.conn.cursor()
+
+    def __exit__(self, type, value, traceback):
+        self.conn.commit()
+
